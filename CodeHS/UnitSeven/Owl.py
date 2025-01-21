b68f3e687fd5faa3aa9a61f122d40beb7e74fe69
ftcaarya <rautaarya23@gmail.com>
2025-01-21 12:41:09 -0500

    7.3.7 Owls

---
+++ CodeHS/UnitSeven/Owl.py
@@ -0,0 +1,10 @@
+def owl_count(text):
+    counter = 0
+    textSplit = text.split()
+    for word in textSplit:
+        for j, letter in enumerate(word):
+            if letter == "o" or letter =="O":
+                if j + 3 <= len(word):
+                    if word[j + 1] == "w" and word[j + 2] == "l":
+                        counter += 1
+
