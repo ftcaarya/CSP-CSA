e7dac06d14ae3bc27438f6f69350f30b9c5b27c7
ftcaarya <rautaarya23@gmail.com>
2025-02-21 11:47:13 -0500

    yea

--- CodeHS/UnitFour/Inventory.py
+++ CodeHS/UnitFour/Inventory.py
@@ -5,7 +5,7 @@ num_items = STARTING_ITEMS_IN_INVENTORY
 
 while num_items >= 0:
     
-    print(f"We have {num_items} items in inventory.")
+    print(f"We have {num_items:.2f} items in inventory.")
     sub = int(input("How many would you like to buy? "))
         
     if sub <= num_items:
@@ -13,7 +13,7 @@ while num_items >= 0:
             print("All out!")
             break
         else:
-            print(f"Now we have {num_items - sub} left.")
+            print(f"Now we have {num_items - sub:.2f} left.")
             
         num_items -= sub
     else:
