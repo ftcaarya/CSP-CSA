ad0f411602e7a670564332c2fc8820703a9e5003
Aarya Raut <127800765+ftcaarya@users.noreply.github.com>
2025-10-17 21:51:20 -0700

    Delete CodeHS/CSP/UnitSeven/HowManyNames?.py

--- CodeHS/CSP/UnitSeven/HowManyNames?.py
+++
@@ -1,9 +0,0 @@
-numNames = int(input("Number of names: "))
-
-names = []
-for i in range(numNames):
-    names.append(input("Name: "))
-
-print(f"First name: {names[0]}")
-print(f'Middle names: {names[1:(len(names) - 1)]}')
-print(f'Last name: {names[len(names) - 1]}')
