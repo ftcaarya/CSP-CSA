e50e7be3d1d81302e704a21f4a58ef0dd70bafe4
Aarya Raut <127800765+ftcaarya@users.noreply.github.com>
2025-10-17 21:47:23 -0700

    Delete CodeHS/CSP/UnitFour/CanYouGraduate?.py

--- CodeHS/CSP/UnitFour/CanYouGraduate?.py
+++
@@ -1,8 +0,0 @@
-# Enter your code here
-
-has_enough_units = False
-has_met_requirements = True
-
-can_graduate = has_enough_units and has_met_requirements
-    
-print("Can graduate? ", can_graduate)
